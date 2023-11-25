68ab618839541ef3800948c484da4bbe378a744d
D Hoyt <h02332@gmail.com>
2023-11-25 11:11:39 -0500

    Updates for iOSOnMac
    # Updates
    
    This Pull Request does the following:
    - Updates the Makefile to Build the Example Project on macOS 14.1.1 | 23B81
    - Adds some print statements to runner
    - Removes legacy compile comments at top of interpose.c
    
    ## iOS App Tree Example
    ```
    tree main.app
    main.app
    ├── Info.plist
    ├── _CodeSignature
    │   └── CodeResources
    └── main
    
    2 directories, 3 files
    ```
    
    ## Testing with Example Code
    ```
    /runner main.app/main
    [+] Child process created with pid: 42525
    [*] Instrumenting process with PID 42525...
    [*] Attempting to attach to task with PID 42525...
    [+] Successfully attached to task with PID 42525
    [*] Finding patch point...
    [*] _amfi_check_dyld_policy_self at offset 0x6e728 in /usr/lib/dyld
    [*] Attaching to target process...
    [*] Scanning for /usr/lib/dyld in target's memory...
    [*] /usr/lib/dyld mapped at 0x104348000
    [*] Patching _amfi_check_dyld_policy_self...
    [+] Sucessfully patched _amfi_check_dyld_policy_self
    [*] Sending SIGCONT to continue child
    Hello World from iOS!
    [*] Child exited with status 0
    ```
    
    ### Testing with Apple Security Research Device Tools | Release 20C80
    ```
    ./runner hello.app/hello
    [+] Child process created with pid: 42536
    [*] Instrumenting process with PID 42536...
    [*] Attempting to attach to task with PID 42536...
    [+] Successfully attached to task with PID 42536
    [*] Finding patch point...
    [*] _amfi_check_dyld_policy_self at offset 0x6e728 in /usr/lib/dyld
    [*] Attaching to target process...
    [*] Scanning for /usr/lib/dyld in target's memory...
    [*] /usr/lib/dyld mapped at 0x104970000
    [*] Patching _amfi_check_dyld_policy_self...
    [+] Sucessfully patched _amfi_check_dyld_policy_self
    [*] Sending SIGCONT to continue child
    Hello researcher from pid 42536!
    [*] Child exited with status 0
    ```
    ### Simple-Server Example | 20C80
    ```
    lldb -- ./runner simple-server.app/simple-server
    (lldb) target create "./runner"
    Current executable set to '/Users/xss/tmp/iOSOnMac/runner' (arm64).
    (lldb) settings set -- target.run-args  "simple-server.app/simple-server"
    (lldb) r
    Process 28561 launched: '/Users/xss/tmp/iOSOnMac/runner' (arm64)
    [+] Child process created with pid: 28565
    [*] Instrumenting process with PID 28565...
    [*] Attempting to attach to task with PID 28565...
    [+] Successfully attached to task with PID 28565
    [*] Finding patch point...
    [*] _amfi_check_dyld_policy_self at offset 0x6e728 in /usr/lib/dyld
    [*] Attaching to target process...
    [*] Scanning for /usr/lib/dyld in target's memory...
    [*] /usr/lib/dyld mapped at 0x100018000
    [*] Patching _amfi_check_dyld_policy_self...
    [+] Sucessfully patched _amfi_check_dyld_policy_self
    [*] Sending SIGCONT to continue child
    2023-11-25 10:00:38.526027-0500 simple-server[28565:155085] [simple-server] Hello! I'm simple-server from the example cryptex!
    2023-11-25 10:00:38.526057-0500 simple-server[28565:155085] [simple-server] I'm about to bind to 0.0.0.0:7777
    2023-11-25 10:00:38.526125-0500 simple-server[28565:155085] [simple-server] I'm about to listen on fd: 3
    2023-11-25 10:00:38.526143-0500 simple-server[28565:155085] [simple-server] Waiting for a client to connect...
    2023-11-25 10:00:43.143281-0500 simple-server[28565:155085] [simple-server] A client has connected!
    2023-11-25 10:00:43.143323-0500 simple-server[28565:155085] [simple-server] Hello! I'm process 28565
    2023-11-25 10:00:43.143363-0500 simple-server[28565:155085] [simple-server] Waiting for a client to connect...
    ```
    #### Simple-Server Check | 20C80 Example
    ```
    telnet 127.0.0.1 7777
    Trying 127.0.0.1...
    Connected to localhost.
    Escape character is '^]'.
    Hello! I'm process 28668
    The environment variable CRYPTEX_MOUNT_PATH  contains: "/Users/xss/Documents/iphone11/com.example.cryptex.dstroot/usr/bin/sh"
    ```
    ### Platform Checks
    ```
    otool -l main.app/main | grep platform
     platform 2
    otool -l hello.app/hello | grep platform
     platform 2
    otool -l interpose.dylib | grep platform
     platform 2
    otool -l runner | grep platform
     platform 1
    ```

--- iOSOnMac/interpose.c
+++ iOSOnMac/interpose.c
@@ -1,5 +1,3 @@
-// clang interpose.c -arch arm64 -o interpose.dylib -shared -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
-
 #include <stdio.h>
 #include <unistd.h>
 
@@ -10,32 +8,34 @@ extern void xpc_dictionary_set_value(xpc_object_t, const char*, xpc_object_t);
 extern xpc_object_t xpc_bool_create(int);
 extern xpc_object_t xpc_copy_entitlements_for_self();
 
-// From https://opensource.apple.com/source/dyld/dyld-97.1/include/mach-o/dyld-interposing.h.auto.html
-/*
- *  Example:
- *
- *  static
- *  int
- *  my_open(const char* path, int flags, mode_t mode)
- *  {
- *    int value;
- *    // do stuff before open (including changing the arguments)
- *    value = open(path, flags, mode);
- *    // do stuff after open (including changing the return value(s))
- *    return value;
- *  }
- *  DYLD_INTERPOSE(my_open, open)
- */
+#define DEBUG_LOGGING 1
 
-#define DYLD_INTERPOSE(_replacment,_replacee) \
-   __attribute__((used)) static struct{ const void* replacment; const void* replacee; } _interpose_##_replacee \
-            __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacment, (const void*)(unsigned long)&_replacee };
+#if DEBUG_LOGGING
+#define DEBUG_PRINT(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
+
+#define DYLD_INTERPOSE(_replacement, _replacee) \
+    __attribute__((used)) static struct { const void* replacement; const void* replacee; } _interpose_##_replacee \
+        __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacement, (const void*)(unsigned long)&_replacee };
 
 xpc_object_t my_xpc_copy_entitlements_for_self() {
-    puts("[*] Faking com.apple.private.security.no-sandbox entitlement in interposed xpc_copy_entitlements_for_self");
+    DEBUG_PRINT("[*] Entering interposed xpc_copy_entitlements_for_self\n");
+
+    // Create a new XPC dictionary
     xpc_object_t dict = xpc_dictionary_create(NULL, NULL, 0);
+    if (!dict) {
+        DEBUG_PRINT("[!] Error creating XPC dictionary\n");
+        return NULL; // Handle error gracefully
+    }
+
+    // Set custom entitlement value
     xpc_dictionary_set_value(dict, "com.apple.private.security.no-sandbox", xpc_bool_create(1));
+    DEBUG_PRINT("[*] Modified entitlements dictionary\n");
+
     return dict;
 }
 
 DYLD_INTERPOSE(my_xpc_copy_entitlements_for_self, xpc_copy_entitlements_for_self);
+
